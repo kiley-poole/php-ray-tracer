4f1c79486d857141f33643dd974b592922d3e073
Kiley Poole <kpoole007@gmail.com>
2021-12-15 19:00:09 -0800

    matrix tests

--- src/Matrix.php
+++ src/Matrix.php
@@ -2,7 +2,7 @@
 
 class Matrix
 {
-    static public function compare(array $matrix_a, array $matrix_b)
+    static public function compare(array $matrix_a, array $matrix_b): bool
     {
         if(count($matrix_a) != count($matrix_b) || count($matrix_a[0]) != count($matrix_b[0])){return false;}
 
@@ -14,7 +14,38 @@ class Matrix
                 if(!Util::floatEquality($matrix_a[$i][$j], $matrix_b[$i][$j])){return false;}
             }
         }
-        
+
         return true;
+    }
+
+    static public function multiply(array $matrix_a, array $matrix_b): array
+    {
+        $a_rows = count($matrix_a);
+        $a_cols = count($matrix_a[0]);
+        $b_cols = count($matrix_b[0]);
+        
+        $matrix_c = array_fill(0, count($matrix_a), array_fill(0, count($matrix_b[0]), 0));
+
+        for ($i = 0; $i < $a_rows; $i++) {
+            for ($j = 0; $j < $a_cols; $j++) {
+              for ($k = 0; $k < $b_cols; $k++)
+                $matrix_c[$i][$k] += $matrix_a[$i][$j] * $matrix_b[$j][$k];
+          }
+        }
+
+        return $matrix_c;
+    }
+
+    static public function transpose(array $matrix): array
+    {
+        $transposed_matrix = [];
+        
+        for($i = 0; $i < count($matrix); $i++){
+            for($j = 0; $j < count($matrix[$i]); $j++){
+                $transposed_matrix[$j][$i] = $matrix[$i][$j];
+            }
+        }
+
+        return $transposed_matrix;
     }
 }
--- tests/MatrixTest.php
+++ tests/MatrixTest.php
@@ -71,6 +71,137 @@ final class MatrixTest extends TestCase
             [8,9,10,11],
             [12,13,14,15]
         ];
+
         $this->assertFalse(Matrix::compare($matrix_a, $matrix_b));
+    }
+
+    public function testMultiplyTwoMatrices()
+    {
+        $matrix_a = [
+            [1,2,3,4],
+            [5,6,7,8],
+            [9,8,7,6],
+            [5,4,3,2]
+        ];
+        $matrix_b = [
+            [-2, 1, 2, 3],
+            [3, 2, 1, -1],
+            [4, 3, 6, 5],
+            [1, 2, 7, 8]
+        ];
+
+        $matrix_c = Matrix::multiply($matrix_a, $matrix_b);
+
+        $this->assertEquals($matrix_c[0][0], 20);
+        $this->assertEquals($matrix_c[0][1], 22);
+        $this->assertEquals($matrix_c[0][2], 50);
+        $this->assertEquals($matrix_c[0][3], 48);
+        $this->assertEquals($matrix_c[1][0], 44);
+        $this->assertEquals($matrix_c[1][1], 54);
+        $this->assertEquals($matrix_c[1][2], 114);
+        $this->assertEquals($matrix_c[1][3], 108);
+        $this->assertEquals($matrix_c[2][0], 40);
+        $this->assertEquals($matrix_c[2][1], 58);
+        $this->assertEquals($matrix_c[2][2], 110);
+        $this->assertEquals($matrix_c[2][3], 102);
+        $this->assertEquals($matrix_c[3][0], 16);
+        $this->assertEquals($matrix_c[3][1], 26);
+        $this->assertEquals($matrix_c[3][2], 46);
+        $this->assertEquals($matrix_c[3][3], 42);
+    }
+
+    public function testMultiplyMatrixAndTuple()
+    {
+        $matrix = [
+            [1,2,3,4],
+            [2,4,4,2],
+            [8,6,4,1],
+            [0,0,0,1]
+        ];
+        $tuple_matrix = [
+            [1],
+            [2],
+            [3],
+            [1]
+        ];
+        $result_matrix = Matrix::multiply($matrix, $tuple_matrix);
+        
+        $this->assertEquals($result_matrix[0][0], 18);
+        $this->assertEquals($result_matrix[1][0], 24);
+        $this->assertEquals($result_matrix[2][0], 33);
+        $this->assertEquals($result_matrix[3][0], 1);
+    }
+
+    public function testIdentityMatrixMultiplication()
+    {
+        $identity_matrix = [
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ];
+
+        $matrix = [
+            [0,1,2,4],
+            [1,2,4,8],
+            [2,4,8,16],
+            [4,8,16,32]
+        ];
+
+        $result_matrix = Matrix::multiply($identity_matrix, $matrix);
+        $this->assertTrue(Matrix::compare($matrix, $result_matrix));
+    }
+
+    public function testIdentityMatrixAndTupleMultiplication()
+    {
+        $identity_matrix = [
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ];
+
+        $tuple_matrix = [
+            [1],
+            [2],
+            [3],
+            [4]
+        ];
+
+        $result_matrix = Matrix::multiply($identity_matrix, $tuple_matrix);
+        $this->assertTrue(Matrix::compare($tuple_matrix, $result_matrix));
+    }
+
+    public function testMatrixTranspose()
+    {
+        $matrix = [
+            [0,9,3,0],
+            [9,8,0,8],
+            [1,8,5,3],
+            [0,0,5,8]
+        ];
+
+        $transposed_matrix = [
+            [0,9,1,0],
+            [9,8,8,0],
+            [3,0,5,5],
+            [0,8,3,8]
+        ];
+
+        $this->assertTrue(Matrix::compare($transposed_matrix, Matrix::transpose($matrix)));
+    }
+
+    public function testTransposeIdentityMatrix()
+    {
+        $identity_matrix = [
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ];
+
+        $transposed_identity_matrix = Matrix::transpose($identity_matrix);
+
+        $this->assertTrue(Matrix::compare($identity_matrix, $transposed_identity_matrix));
     }
 }
